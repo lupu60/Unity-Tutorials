7aabfecbe4af49d014e2fe39c46caa1909300138
Lupu Bogdan <lupu60@gmail.com>
2015-03-17 18:48:24 +0200

    Roll-a-Ball Finish + texture improve.

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
 	// Use this for initialization
 	void Start () {
-	
+		count = 0;	
+		SetCountText ();
+		winText.text = "";
+
 	}
 	
 	// Update is called once per frame
@@ -17,5 +24,18 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rigidbody.AddForce (movement * speed *Time.deltaTime); 
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			count++;
+			other.gameObject.SetActive(false);
+			SetCountText ();
+		}
+	}
+	void SetCountText(){
+		countText.text = "Count  " + count.ToString ();
+		if (count >= 13) {
+			winText.text = "YOU WIN!";
+		}
 	}
 }
