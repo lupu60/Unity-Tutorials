b2207f41746b0526d005e38892e276fdfd3bbb4f
Bogdan Lupu <lupu60@gmail.com>
2015-04-02 00:18:41 +0300

    ..

---
+++ Stealth/Assets/FindTheBall.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindTheBall : MonoBehaviour {
+	Transform player;
+	NavMeshAgent nav;
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Find").transform;
+
+		nav = GetComponent <NavMeshAgent>();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination(player.position);
+	}
+}
